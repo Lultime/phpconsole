de4610a36c1d97fbc536bf1dbfdfae75267bf0d5
Lultime <117547665+Lultime@users.noreply.github.com>
2022-11-25 21:10:01 -0500

    Add files via upload

---
+++ fonction.php
@@ -0,0 +1,15 @@
+<?php
+$a=10;
+$b=7;
+$s=$a * $b;
+
+function multiplication(){
+$a=7;
+$b=5;
+$s=$a*$b;
+echo"la somme de $a par $b est $s: ";
+
+}
+multiplication ();
+echo"la somme de $a par $b est $s:";
+?>
