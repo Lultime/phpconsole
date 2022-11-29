33c56fe2d6254ae2c0e3a8ab60fde0798be06770
Lultime <117547665+Lultime@users.noreply.github.com>
2022-11-28 22:04:08 -0500

    Add files via upload

---
+++ PROJET/fonctions.php
@@ -0,0 +1,26 @@
+<?php
+$tableau=[10,23,66,11];
+$fonction= count($tableau);
+$fonctin2=array_sum();
+$fonction3=strrev();
+$fonction4=strtolower();
+print_r();
+var_dump();
+$fonction4=array_push();
+
+while(true){
+    $mot=readline('entrer un mot');
+      
+    if ($mot===''){
+        exit ('fin du programme');
+    }
+      
+
+}
+
+
+
+
+
+
+?>
